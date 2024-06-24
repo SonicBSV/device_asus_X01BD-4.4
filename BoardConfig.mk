d9ab29f08d8dcec72cbb676d5a7f115fb2bbd109
SonicBSV <sonicbsv@gmail.com>
2024-06-24 22:41:13 +0300

    X01BD: update & merge * merge from https://github.com/SonicBSV/android_device_asus_X01BD-4.19/commits/lineage-21.0/

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,9 +21,14 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
 TARGET_KERNEL_CONFIG := X01BD_defconfig
+TARGET_KERNEL_BUILD_HOST := kgb
+TARGET_KERNEL_BUILD_USER := "SonicBSV"
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit the proprietary files
 include vendor/asus/X01BD/BoardConfigVendor.mk
